faccdd800a8d4d067b7081f3eec1c19e3e478f85
Ganesh Rajendran <rganesh777@gmail.com>
2025-12-15 18:47:13 +1100

    Update readme

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig(() => ({
+  base: '/Checkbox-workflow-react-challenge/',
   server: {
     host: 'localhost',
     port: 5173,
